4d5ac9a1a5aff414c927062f5a0cc07d371f52e4
Ari0307 <arina.zubkova@mail.ru>
2017-05-10 22:03:32 +0300

    Create WantToReadTableViewController.swift

---
+++ WantToReadTableViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class WantToReadTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var booksTableView: UITableView!
+    @IBOutlet weak var addButton: UIButton!
+    
+        private var books = [Book]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as! WantToReadTableViewCell
+        cell.bookNameLabel.text = books[indexPath.item].author + " " + books[indexPath.item].title
+        cell.infoButton.addTarget(self, action: #selector(showNotesViewController(_:)), for: .touchUpInside)
+        return cell
+    }
+    @IBAction func addRow(_ sender: Any) {
+        books.append(Book(author: "Пушкин", title: "Капитанская дочка"))
+        booksTableView.beginUpdates()
+        booksTableView.insertRows(at: [IndexPath(row: books.count - 1, section: 0)], with: .automatic)
+        booksTableView.endUpdates()
+    }
+    
+    func showNotesViewController(_ sender: UIButton){
+        self.performSegue(withIdentifier: "ShowNotesViewController", sender: self)
+    }
+
+}
