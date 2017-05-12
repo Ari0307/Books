c4b63796e84e50f404504d784524cc4e2a5b8d61
Ari0307 <arina.zubkova@mail.ru>
2017-05-12 11:03:10 +0300

    NotesViewController.swift

---
+++ NotesViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class NotesViewController: UIViewController {
+
+    
+    @IBOutlet weak var notesTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
